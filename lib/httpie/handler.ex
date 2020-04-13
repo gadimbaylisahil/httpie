2f9fc4669bc7d85a3a8d7f8333b826f64d010ac9
Sahil Gadimbayli <sahil.gadimbay@gmail.com>
2020-04-13 20:20:52 +0200

    add catch all route

--- lib/httpie/handler.ex
+++ lib/httpie/handler.ex
@@ -1,7 +1,8 @@
 defmodule Httpie.Handler do
   def handle(request) do
     request 
-      |> parse 
+      |> parse
+      |> log 
       |> route 
       |> format_response
   end
@@ -13,16 +14,35 @@ defmodule Httpie.Handler do
       |> List.first
       |> String.split(" ")
     
-    %{method: method, path: path, res_body: ""}
+    %{
+      method: method, 
+      path: path, 
+      res_body: "",
+      status: nil
+      }
   end
 
+  def log(conv), do: IO.inspect conv
+
   def route(conv) do
-    %{conv | res_body: "Product1, Product2, Product2"}
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/products") do
+    %{conv | status: "200 OK", res_body: "Product1, Product2, Product2"}
+  end
+
+  def route(conv, "GET", "/users") do
+    %{conv | status: "200 OK", res_body: "User1, User2, User3"} 
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: "404 Not Found", res_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
     """
-      HTTP/1.1 200 OK
+      HTTP/1.1 #{conv.status}
       Content-Type: text/html
       Content-Length: #{String.length(conv.res_body)}
 
@@ -32,7 +52,7 @@ defmodule Httpie.Handler do
 end
 
 request = """
-GET /wildthings HTTP/1.1
+GET /products HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
