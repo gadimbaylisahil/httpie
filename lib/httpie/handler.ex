9a7cd393cabb4dba6ff64e58476eaef527ced1c1
Sahil Gadimbayli <sahil.gadimbay@gmail.com>
2020-04-13 20:24:38 +0200

    add status reason code func

--- lib/httpie/handler.ex
+++ lib/httpie/handler.ex
@@ -29,25 +29,36 @@ defmodule Httpie.Handler do
   end
 
   def route(conv, "GET", "/products") do
-    %{conv | status: "200 OK", res_body: "Product1, Product2, Product2"}
+    %{conv | status: 200, res_body: "Product1, Product2, Product2"}
   end
 
   def route(conv, "GET", "/users") do
-    %{conv | status: "200 OK", res_body: "User1, User2, User3"} 
+    %{conv | status: 200, res_body: "User1, User2, User3"} 
   end
 
   def route(conv, _method, path) do
-    %{ conv | status: "404 Not Found", res_body: "No #{path} here!"}
+    %{ conv | status: 404, res_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
     """
-      HTTP/1.1 #{conv.status}
+      HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
       Content-Type: text/html
       Content-Length: #{String.length(conv.res_body)}
 
       #{conv.res_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
